68502b4a181d3c79876fcd63c4ab3d5de29d8070
Robin Ahn <robin.ahn99@gmail.com>
2024-11-19 10:32:23 +0100

    Basic Routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./style.scss";
+
+// Components
 import App from "./App.jsx";
+import Profile from "./pages/Profile.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<App />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </Router>
     </StrictMode>,
 );
