50a919e2d780538bbfd52d6645d6199fb87a4959
Marfien <contact@marfien.dev>
2024-11-26 17:55:31 +0100

    Fixed deployment scripts

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -1,7 +1,7 @@
-#!/bin/env bash
+#!/bin/bash
 
 export APPLICATION_COMPONENT="frontend"
 # We are storing the curl result in a variable because otherwise the stdin would be closed and the eval would not work resulting in a "curl: (23) Failed writing body" error
-setup_script=$(curl --fail https://raw.githubusercontent.com/food2gether/flux-base/refs/heads/main/bin/minikube-setup.sh)
+setup_script=$(curl --silent --fail https://raw.githubusercontent.com/food2gether/flux-base/refs/heads/main/bin/minikube-setup.sh)
 eval "$setup_script"
 
