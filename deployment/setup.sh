92dfc748dc3c824846eb3f7df2c1bd4e29c97fc3
Marvin Haase <marvin.haase@soptim.de>
2024-11-28 11:08:25 +0100

    Only providing bash script

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export APPLICATION_COMPONENT="frontend"
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <github_pat>"
+    exit 1
+fi
+
 # We are storing the curl result in a variable because otherwise the stdin would be closed and the eval would not work resulting in a "curl: (23) Failed writing body" error
 setup_script=$(curl --silent --fail https://raw.githubusercontent.com/food2gether/flux-base/refs/heads/main/bin/minikube-setup.sh)
-eval "$setup_script"
-
+echo "$setup_script" | bash /dev/stdin "$1" "frontend"
