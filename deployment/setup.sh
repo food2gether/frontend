f587db2a89bc63e945b5740a1e3a3e67769c371d
Marvin Haase <marvin.haase@soptim.de>
2024-11-29 14:44:39 +0100

    Adjusted deployment config

--- deployment/setup.sh
+++ deployment/setup.sh
@@ -6,5 +6,4 @@ if [ "$#" -ne 1 ]; then
 fi
 
 # We are storing the curl result in a variable because otherwise the stdin would be closed and the eval would not work resulting in a "curl: (23) Failed writing body" error
-setup_script=$(curl --silent --fail https://raw.githubusercontent.com/food2gether/flux-base/refs/heads/main/bin/minikube-setup.sh)
-echo "$setup_script" | bash /dev/stdin "$1" "frontend"
+curl --silent --fail https://raw.githubusercontent.com/food2gether/flux-base/refs/heads/main/bin/minikube-setup.sh | bash /dev/stdin "$1" "frontend"
