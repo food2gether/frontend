e6e135956bab974ae1c166741399f912d568c556
Marfien <contact@marfien.dev>
2025-03-19 20:39:25 +0100

    feat: option for inline input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Input({ type, placeholder, defaultValue, onChange, valid = true, className }) {
+function Input({ type, placeholder, defaultValue, onChange, valid = true, inline, className }) {
     return (
         <input
             type={type}
             placeholder={placeholder}
-            className={`border-b-2 rounded-t-lg py-[5px] px-[10px] outline-none text-black text-[20px] focus:bg-opacity-5 focus:bg-black transition ${valid ? "border-black focus:border-primary" : "border-red-500"} ${className}`}
+            className={`border-b-2 rounded-t-lg outline-none ${inline ? "text-inherit" : "text-black text-sm py-1 px-2"}  focus:bg-opacity-5 focus:bg-black transition ${valid ? "border-black focus:border-primary" : "border-red-500"} ${className}`}
             defaultValue={defaultValue}
             onChange={onChange}
         />
